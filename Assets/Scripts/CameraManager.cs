3373154e8521c88d0e27ba97200362498df22b40
yozakurasun <0925maki.hosaka1222@gmail.com>
2026-01-17 10:38:15 +0900

    [modify] 果物が画面外に落下事カメラが揺れる演出追加、ゲームオーバー時のUIアニメーション

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+
+public class CameraManager : MonoBehaviour
+{
+    private Vector3 _defaultStrength = new Vector3(0.6f, 0.2f, 0f);
+
+    /// <summary>
+    /// ƒJƒƒ‰‚ª—h‚ê‚é‰‰o
+    /// </summary>
+    /// <param name="duration"></param>
+    /// <param name="strength"></param>
+    /// <param name="vibrato"></param>
+    /// <param name="randomness"></param>
+    /// <param name="snapping"></param>
+    /// <param name="fadeOut"></param>
+    /// <returns></returns>
+    public async UniTask CameraShakeEvent(float duration = 1f, Vector3 strength = default, int vibrato = 10, float randomness = 90, bool snapping = false, bool fadeOut = true)
+    {
+        if (strength == default)
+        {
+            strength = _defaultStrength;
+        }
+        await transform.DOShakePosition(duration, strength, vibrato, randomness, snapping, fadeOut).ToUniTask();
+    }
+}
